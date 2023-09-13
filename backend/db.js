4f3f9e76b72ac8ef95455ea2f36ec44bd394238d
MayurSorathiya77 <133674110+MayurSorathiya77@users.noreply.github.com>
2023-09-13 13:06:52 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose =require('mongoose')
  
 const urlLocal = 'mongodb://127.0.0.1:27017/stacoverflow';
-const url = "mongodb+srv://mayursorathiya107:ptlh1qThfv4TBJsn@stackdb.ubjqxdo.mongodb.net/stackoverflowclone?retryWrites=true&w=majority"
+//const url = "mongodb+srv://mayursorathiya107:<password>@stackdb.ubjqxdo.mongodb.net/<databaseName>?retryWrites=true&w=majority"
 module.exports.connect = ()=>
 {
     mongoose.connect(urlLocal)
@@ -9,5 +9,3 @@ module.exports.connect = ()=>
     .catch((err)=>console.log('Error  dd'))
 }
 
-//ptlh1qThfv4TBJsn            //password
-//mayursorathiya107 
